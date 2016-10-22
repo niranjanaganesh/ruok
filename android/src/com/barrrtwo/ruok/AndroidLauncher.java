4a6ea6fa63aedcbfcfe1bf8174c8f2a533d6cf5f
Josh Siegel <JoshuaScottSoftware@gmail.com>
2016-10-22 15:59:06 -0400

    Second try

---
+++ android/src/com/barrrtwo/ruok/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.barrrtwo.ruok;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.barrrtwo.ruok.FirstAidMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FirstAidMain(), config);
+	}
+}
---
+++ desktop/src/com/barrrtwo/ruok/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.barrrtwo.ruok.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.barrrtwo.ruok.FirstAidMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FirstAidMain(), config);
+	}
+}
