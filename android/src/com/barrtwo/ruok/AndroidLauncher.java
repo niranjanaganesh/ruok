f0b15f373f50a6823ce5067526300e5851a50d81
Josh Siegel <JoshuaScottSoftware@gmail.com>
2016-10-22 14:33:35 -0400

    Initial Framework for our app

---
+++ android/src/com/barrtwo/ruok/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.barrtwo.ruok;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.barrtwo.ruok.FirstAidMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FirstAidMain(), config);
+	}
+}
---
+++ desktop/src/com/barrtwo/ruok/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.barrtwo.ruok.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.barrtwo.ruok.FirstAidMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FirstAidMain(), config);
+	}
+}
